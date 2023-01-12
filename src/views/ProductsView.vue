9dee486eca1aa822189ebfa1e95f66c9b8934777
mccp <parent.clement2@gmail.com>
2023-01-12 16:29:11 +0100

    [feature] - Add checkboxes to ProductsView

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="products-view">
     <div class="container">
-      <h1 class="products-view__title">Mes produits</h1>
       <div class="row">
         <div class="column -size-3">
-          <!-- Filtres -->
+          {{ filters }}
+          <div class="products-view__checkbox">
+            <input v-model="filters" type="checkbox" id="cuisine" name="cuisine" value="cuisine">
+            <label for="cuisine">Cuisine</label>
+          </div>
+          <div class="products-view__checkbox">
+            <input v-model="filters" type="checkbox" id="chambre" name="chambre" value="chambre">
+            <label for="chambre">Chambre</label>
+          </div>
+          <div class="products-view__checkbox">
+            <input v-model="filters" type="checkbox" id="salon-sejour" name="salon-sejour" value="salon-sejour">
+            <label for="salon-sejour">Salon/Séjour</label>
+          </div>
         </div>
         <div class="column -size-9">
+          <h1 class="products-view__title">Mes produits</h1>
           <div class="products-view__list">
             <div class="row">
               <div class="column -size-3" v-for="(product, index) in filteredProducts" :key="index">
@@ -28,7 +40,8 @@ export default {
   components: { Product },
   data () {
     return {
-      products: []
+      products: [],
+      filters: []
     }
   },
 
@@ -44,6 +57,12 @@ export default {
     // Get all woocommerce products
     const productResponse = await client.get("/wc/v3/products")
     this.products = productResponse.data
+  },
+
+  methods: {
+    onChange ($event) {
+      console.log($event)
+    }
   }
 };
 </script>
