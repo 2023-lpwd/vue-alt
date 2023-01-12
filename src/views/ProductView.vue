a8618e083d0a9e22da2c3c9d419482bb0c842ebd
mccp <parent.clement2@gmail.com>
2023-01-12 13:52:37 +0100

    [style] - Add styles to ProductView

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="product-view">
     <div class="container">
-      <h1 class="product-view__name">{{ product.name }}</h1>
+      <div class="row">
+        <div class="column -size-6">
+          <!-- Galerie d'images -->
+        </div>
+        <div class="product-view__content | column -size-6">
+          <h1 class="product-view__name">{{ product.name }}</h1>
+          <span class="product-view__sku">{{ product.sku }}</span>
+          <p class="product-view__price">{{ product.price }}€</p>
+          <div class="product-view__description">
+            <div v-if="product.dimensions" class="product-view__dimensions">
+              <p class="product-view__subtitle">Dimensions du produit :</p>
+              <ul>
+                <li class="product-view__dimensions-item">Longueur : {{ product.dimensions.length }}cm</li>
+                <li class="product-view__dimensions-item">Largeur : {{ product.dimensions.width }}cm</li>
+                <li class="product-view__dimensions-item">Hauteur : {{ product.dimensions.height }}cm</li>
+              </ul>
+            </div>
+            <p class="product-view__subtitle">Description du produit :</p>
+            <div class="product-view__description-content" v-html="product.short_description" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,5 +56,33 @@ export default {
 </script>
 
 <style lang="scss">
+.product-view {
 
+  &__content {
+    padding-top: 50px;
+    padding-bottom: 50px;
+  }
+
+  &__name {
+    margin: 0;
+  }
+
+  &__sku {
+    display: inline-block;
+    margin: 5px 0 0 0;
+  }
+
+  &__price {
+    font-size: 25px;
+    font-weight: 700;
+    color: $secondary-color
+  }
+
+  &__subtitle {
+    font-size: 20px;
+    font-weight: 700;
+    color: $secondary-color;
+  }
+
+}
 </style>
